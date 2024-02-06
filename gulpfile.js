5ff8b054a81916418dddf8f4fb01d053320cf412
Daniel Larsson <eadaniel.larsson@gmail.com>
2024-02-06 15:15:50 +0200

    finalized and ran gulp with watch tasks successfully

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,52 @@
 
 import gulp from 'gulp';
 import htmlmin from 'gulp-htmlmin';
+import sassPackage from 'sass';
+import gulpSass from 'gulp-sass';
 import cleanCSS from 'gulp-clean-css';
 import uglify from 'gulp-uglify';
-import sass from 'gulp-sass';
 import imagemin from 'gulp-imagemin';
+import concat from 'gulp-concat';
 
-// Minify HTML
-function minifyHTML() {
-  return gulp.src('src/*html')
+const sass = gulpSass(sassPackage);
+
+export function minifyHTML() {
+  return gulp.src('src/*.html')
       .pipe(htmlmin({ collapseWhitespace: true }))
       .pipe(gulp.dest('dist'));
 }
 
-// Minify CSS
-function minifyCSS() {
-  return gulp.src('src/styles/*.scss')
+export function compileSass() {
+  return gulp.src('src/styles/**/*.scss')
+    .pipe(sass())
+    .pipe(concat('main.css'))
+    .pipe(gulp.dest('src/css'))
+}
+
+export function minifyCSS() {
+  return gulp.src('src/css/*.css')
     .pipe(cleanCSS())
     .pipe(gulp.dest('dist/styles'));
 }
 
-// Minify JS
-function minifyJS() {
-  
+export function minifyJS() {
+  return gulp.src('src/scripts/*js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/scripts'));
 }
+
+export function optimizeImages() {
+  return gulp.src('src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/images'))
+}
+
+export function watch() {
+  gulp.watch('src/*.html', minifyHTML);
+  gulp.watch('src/styles/*.scss', compileSass);
+  gulp.watch('src/styles/*.css', minifyCSS);
+  gulp.watch('src/scripts/*js', minifyJS);
+  gulp.watch('src/images/*', optimizeImages);
+}
+
+export default gulp.series(minifyHTML, compileSass, minifyCSS, minifyJS, optimizeImages, watch);
